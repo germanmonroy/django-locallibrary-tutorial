84b6c35a1a42adab30a1392b430ee187dd26198e
Germán Monroy <germandresmonroy@gmail.com>
2020-09-28 16:16:13 -0400

    class generic views, urls, and templates for author list and detail pages

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,7 +4,11 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('books/', views.BookListView.as_view(), name='books'),
-    # path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+
     # regex version
-    re_path(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
+    # re_path(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
+
+    path('authors/', views.AuthorListView.as_view(), name='authors'),
+    path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
 ]
